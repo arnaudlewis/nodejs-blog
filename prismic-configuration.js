d5f00b55d20490108d4fe9eb1fe2312fb5cd20c9
Arnaud lewis <alw@wengularity.com>
2016-10-31 11:35:30 +0100

    simplify linkResolver

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -9,13 +9,6 @@ exports.Configuration = {
   // This function will be used to generate links to Prismic.io documents
   // As your project grows, you should update this function according to your routes
   linkResolver: function(doc, ctx) {
-    if (doc.type == 'blog') {
-      return '/blog';
-    }
-    if (doc.type == 'post') {
-      return '/blog/' + encodeURIComponent(doc.uid);
-    }
-
     return '/';
   },
   onPrismicError: function(err, req, res) {
